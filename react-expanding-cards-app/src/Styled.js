08eeeba0a66f43fefadff9b649826c6f395911c2
szopian <szopiangreg@gmail.com>
2021-08-25 12:22:11 +0100

    add  work on Panel

---
+++ react-expanding-cards-app/src/Styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const Panel = styled.div``;
+
+export const Active = styled.div``;
+
+export const Heading = styled.h3``;

9657e053c4a38d6f1140b6a650cc0f0fbe5cdded
szopian <szopiangreg@gmail.com>
2021-08-26 13:42:59 +0100

    work in progress

--- react-expanding-cards-app/src/components/Panel.js
+++ react-expanding-cards-app/src/components/Panel.js
@@ -1,26 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Image from "../images/cem-ersozlu-bR9abI2wC88-unsplash.jpg";
 import { Container } from "../Styled";
 
-export default function Panel {
+export default function Panel() {
   const [isActive, setActive] = useState("false");
 
   const handleToggle = () => {
     setActive(!isActive);
   };
 
-  render() {
-    return (
-      <Container>
-        <ImgPanel onClick={handleToggle} className={isActive ? "app" : "container"} />
-        <ImgPanel onClick={handleToggle} className={isActive ? "app" : "container"} />
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <ImgPanel
+        className={isActive ? ImgPanelActive : ImgPanel}
+        onClick={handleToggle}
+      />
+    </Container>
+  );
 }
-
-export default Panel;
 
 const ImgPanel = styled.div`
   background-size: cover;
@@ -32,13 +30,13 @@ const ImgPanel = styled.div`
   cursor: pointer;
   margin: 10px;
   position: relative;
-  /* --webkit-transition: all 700ms ease-in; */
+  --webkit-transition: all 700ms ease-in;
   background-image: url(${Image});
   flex: 0.5;
 `;
 
 const ImgPanelActive = styled(ImgPanel)`
-
+  flex: 5;
 `;
 
 // const Heading = styled.h3``;

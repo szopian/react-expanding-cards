be3201dc57dded409f58bafbf8a77565668b7e76
szopian <szopiangreg@gmail.com>
2021-08-28 17:13:43 +0100

    add changes

--- react-expanding-cards-app/src/App.js
+++ react-expanding-cards-app/src/App.js
@@ -1,6 +1,7 @@
 import Panel from "./components/Panel";
 import { Wrapper, Container } from "./Styled";
 import styled from "styled-components";
+import React, { Component, useState } from "react";
 
 import ImageA from "./images/cem-ersozlu-bR9abI2wC88-unsplash.jpg";
 import ImageB from "./images/jakob-rosen-R3Kt-VGPwUI-unsplash.jpg";
@@ -9,7 +10,7 @@ import ImageD from "./images/matteo-catanese-9o2rxHYRyro-unsplash.jpg";
 import ImageE from "./images/pexels-bryan-gomes-3977529.jpg";
 import ImageF from "./images/pexels-tobias-bjørkli-2119706.jpg";
 
-const ImgPanel = styled.div`
+const ImgPanel = styled.button`
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
@@ -21,15 +22,18 @@ const ImgPanel = styled.div`
   position: relative;
   transition: all 0.7s ease-in;
   background-image: url(${ImageA});
-  flex: 0.5;
 
-  /* &&:active {
-    flex: 5;
-  } */
-
-  &&.activeFlex {
-    flex: 5;
-  }
+  ${(props) => {
+    if (props.toggle) {
+      return `
+      flex: 0.5;
+      `;
+    } else {
+      return `
+      flex: 5;
+      `;
+    }
+  }}
 
   @media (max-width: 480px) {
     &&:nth-of-type(5),
@@ -39,20 +43,53 @@ const ImgPanel = styled.div`
   }
 `;
 
-function App() {
-  return (
-    <Wrapper>
-      <Container>
-        <ImgPanel className="activeFlex" />
-        <Panel BgImg={ImageA} />
-        <Panel BgImg={ImageB} />
-        <Panel BgImg={ImageC} />
-        <Panel BgImg={ImageD} />
-        <Panel BgImg={ImageE} />
-        <Panel BgImg={ImageF} />
-      </Container>
-    </Wrapper>
-  );
+class App extends Component {
+  state = { checkBool: false };
+
+  setBoolTrue() {
+    this.setState({ checkBool: true });
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <Container>
+          <ImgPanel
+            as="button"
+            toggle={this.state.checkBool}
+            onClick={this.state.checkBool}
+          />
+          ;
+          <Panel
+            BgImg={ImageA}
+            toggle={this.state.checkBool}
+            onClick={this.state.checkBool}
+          />
+          <Panel BgImg={ImageB} />
+          <Panel BgImg={ImageC} />
+          <Panel BgImg={ImageD} />
+          <Panel BgImg={ImageE} />
+          <Panel BgImg={ImageF} />
+        </Container>
+      </Wrapper>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <Wrapper>
+//       <Container>
+//         <ImgPanel />
+//         <Panel BgImg={ImageA} />
+//         <Panel BgImg={ImageB} />
+//         <Panel BgImg={ImageC} />
+//         <Panel BgImg={ImageD} />
+//         <Panel BgImg={ImageE} />
+//         <Panel BgImg={ImageF} />
+//       </Container>
+//     </Wrapper>
+//   );
+// }
 
 export default App;
